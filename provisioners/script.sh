49f1a8cc5cd01420822af4aff6597f660909bfd8
Jean-Marc Goepfert <jean-marcgoepfert@192-168-1-5.tpgi.com.au>
2016-04-04 00:07:46 +1000

    Initial commit

---
+++ provisioners/script.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+home=/home/vagrant
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Provisioning virtual machine..."
+
+echo "Updating apt-get..."
+sudo apt-get update -y
+
+if [ ! -d "$home/bin" ]; then
+  echo "Making bin dir in $home..."
+  mkdir -p "$home/bin"
+  chmod a+x $home/bin
+  echo '
+    if [ -d "$HOME/bin" ];
+      then PATH="$PATH:$HOME/bin"
+    fi' >> $home/.bashrc
+fi
+
+echo "Installing ack..."
+sudo apt-get install ack -y
+
+echo "Installing curl..."
+sudo apt-get install curl -y
+
+echo "Installing wget..."
+sudo apt-get install wget -y
+
+echo "Installing git..."
+sudo apt-get install git -y
+
+echo "Installing vim..."
+sudo apt-get install vim -y
+
+if [ ! -d "/usr/lib/jvm/" ]; then
+  echo "Installing java..."
+
+  curl -L --cookie "oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/7u65-b17/jdk-7u65-linux-x64.tar.gz -o jdk-7-linux-x64.tar.gz
+
+  tar -xvf jdk-7-linux-x64.tar.gz
+
+  sudo mkdir -p /usr/lib/jvm
+
+  sudo mv ./jdk1.7.* /usr/lib/jvm/
+
+  sudo update-alternatives --install "/usr/bin/java" "java" "/usr/lib/jvm/jdk1.7.0_65/bin/java" 1
+  sudo update-alternatives --install "/usr/bin/javac" "javac" "/usr/lib/jvm/jdk1.7.0_65/bin/javac" 1
+  sudo update-alternatives --install "/usr/bin/javaws" "javaws" "/usr/lib/jvm/jdk1.7.0_65/bin/javaws" 1
+
+  sudo chmod a+x /usr/bin/java
+  sudo chmod a+x /usr/bin/javac
+  sudo chmod a+x /usr/bin/javaws
+  sudo chown -R root:root /usr/lib/jvm/jdk1.7.0_65
+
+  rm jdk-7-linux-x64.tar.gz
+  rm -f equip_base.sh
+  rm -f equip_java7_64.sh
+fi
+
+if [ ! -f "$home/bin/lein" ]; then
+  echo "Installing leiningen..."
+  wget https://raw.github.com/technomancy/leiningen/stable/bin/lein -O $home/bin/lein --progress=bar:force
+  chmod a+x $home/bin/lein
+fi
